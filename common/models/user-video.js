323068c739db6a6be248148050555755e5441708
Blair Douglass <blair.douglass@gmail.com>
2014-11-19 13:57:49 -0600

    Added performance timing

--- common/models/user-video.js
+++ common/models/user-video.js
@@ -1,17 +1,24 @@
 var ffmpeg = require('fluent-ffmpeg'),
 	fs = require('fs'),
 	child_process = require('child_process'),
-	uuid = require('node-uuid');
+	uuid = require('node-uuid'),
+	now = require("performance-now"),
 	AWS = require('aws-sdk');
 
 module.exports = function(UserVideo) {
 	UserVideo.generate = function(photoName, cb) {
-		console.log("rendering file")
+		var methodStart = now();
 		var userImageFile = "./client/sourceFiles/kitchen_8.jpg";
 		
-		var loopedImageName = "photoName" + uuid.v4() + ".mp4";
+		var loopedImageName = photoName + uuid.v4() + ".mp4";
 		
-		ffmpeg(userImageFile).loop(30).addInput(userImageFile).addOptions(['-c:v libx264', '-c:a aac', '-strict experimental', '-t 30', '-pix_fmt yuv420p']).save('./client/generatedVideos/' + loopedImageName).on('end', function(){console.log('Finished Looping!'); createUserVideo(loopedImageName, cb);});
+		var loopImageStart = now();
+		ffmpeg(userImageFile).loop(30).addInput(userImageFile).addOptions(['-c:v libx264', '-c:a aac', '-strict experimental', '-t 30', '-pix_fmt yuv420p']).save('./client/generatedVideos/' + loopedImageName).on('end', 
+			function(){
+				var loopImageEnd = now();
+				console.log(photoName + " looped image time:" + (loopImageEnd-loopImageStart).toFixed(3));
+				createUserVideo(loopedImageName, cb);
+			});
 
 		//exec('ffmpeg -loop 1 -i '+userImageFile+' -c:v libx264 -c:a aac -strict experimental -t 30 -pix_fmt yuv420p '+ loopedImageName +';',function(){
 			//var newVideo = loopedImageName;
@@ -31,6 +38,7 @@ module.exports = function(UserVideo) {
 
 function createUserVideo(loopedImageName, cb)
 {
+	var userVideoStart = now();
 	var maytagAudioFile = "./client/sourceFiles/audio.aif";
 	var maytagOverlayFile = "./client/sourceFiles/frankerberry_countchockula.mp4";
 	var xCoord = 300;
@@ -38,28 +46,32 @@ function createUserVideo(loopedImageName, cb)
 	var vWidth = 270;
 	var vHeight = 470;
 	
-	console.log("creating user video");
 	var savedImageName = uuid.v4() + ".mp4";
-	ffmpeg('./client/generatedVideos/' + loopedImageName).mergeAdd(maytagAudioFile).mergeAdd('./client/generatedVideos/' + loopedImageName).addOption(['-vf', 'movie='+maytagOverlayFile+ ' [watermark]; [in] [watermark] overlay=shortest=1:x='+xCoord+':y='+yCoord+' [out]']).outputOptions('-metadata', 'title=Bring Maytag Home').save('./client/generatedVideos/' + savedImageName).on('end', function(){console.log('Finished Processing !'); uploadFile(savedImageName, cb);});
+	ffmpeg('./client/generatedVideos/' + loopedImageName).mergeAdd(maytagAudioFile).mergeAdd('./client/generatedVideos/' + loopedImageName).addOption(['-vf', 'movie='+maytagOverlayFile+ ' [watermark]; [in] [watermark] overlay=shortest=1:x='+xCoord+':y='+yCoord+' [out]']).outputOptions('-metadata', 'title=Bring Maytag Home').save('./client/generatedVideos/' + savedImageName).on('end', 
+	function(){
+		var userVideoEnd = now();
+		console.log(loopedImageName + " render time:" + (userVideoEnd-userVideoStart).toFixed(3)); 
+		uploadFile(savedImageName, cb);
+	});
 }
 
 function uploadFile(savedImageName, cb) {
-  var fileBuffer = fs.readFileSync('./client/generatedVideos/' + savedImageName);
-  var metaData = getContentTypeByFile('./client/generatedVideos/' + savedImageName);
-  var s3 = new AWS.S3();
+  	var uploadStart = now();
+  	var fileBuffer = fs.readFileSync('./client/generatedVideos/' + savedImageName);
+  	var metaData = getContentTypeByFile('./client/generatedVideos/' + savedImageName);
+  	var s3 = new AWS.S3();
 
-  s3.putObject({
-    ACL: 'public-read',
-    Bucket: "bmmh-testing",
-    Key: savedImageName,
-    Body: fileBuffer,
-    ContentType: metaData
-  }, function(error, response) {
-    console.log('uploaded file[' + './client/generatedVideos/' + savedImageName + '] to [' + savedImageName + '] as [' + metaData + ']');
-    console.log(arguments);
-    console.log(response);
-	cb(null, 'The video url: ' + 'https://s3-us-west-2.amazonaws.com/bmmh-testing/' + savedImageName);
-  });
+  	s3.putObject({
+    	ACL: 'public-read',
+    	Bucket: "bmmh-testing",
+    	Key: savedImageName,
+    	Body: fileBuffer,
+    	ContentType: metaData
+  	}, function(error, response) {
+		var uploadEnd = now();
+		console.log(savedImageName + " upload time:" + (uploadEnd-uploadStart).toFixed(3));
+		cb(null, 'The video url: ' + 'https://s3-us-west-2.amazonaws.com/bmmh-testing/' + savedImageName);
+  	});
 }
 
 function getContentTypeByFile(fileName) {
